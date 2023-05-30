878381020cfcadc5c35ffed9ca22945b1f48fea4
İsmail Emre Sırmalı <lynicis@protonmail.com>
2023-05-31 01:35:17 +0300

    fix lint

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 
-	"user-api/internal"
+	user "user-api/internal"
 	"user-api/pkg/config"
 	"user-api/pkg/jwt_generator"
 	"user-api/pkg/logger"
