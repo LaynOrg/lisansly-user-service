80aecabf4c2629b4c3183c4277ecf784e4bd6f1c
İsmail Emre Sırmalı <lynicis@protonmail.com>
2023-06-03 00:26:12 +0300

    update cors config for serverless.yaml

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 
 	user "user-api/internal"
@@ -53,7 +52,6 @@ func main() {
 	logMiddleware := logger.Middleware(log)
 	app := srv.GetFiberInstance()
 	app.Use(logMiddleware)
-	app.Use(cors.New())
 	app.Get("/health", func(ctx *fiber.Ctx) error {
 		return ctx.Status(fiber.StatusOK).SendString("OK")
 	})
